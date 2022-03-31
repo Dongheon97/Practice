205cdeae5638ed9054dacd58f9ccae050353fb5d
Dongheon97 <jdd04063@gmail.com>
2022-03-31 17:26:26 +0900

    Federated Learning with Pytorch

--- coding_test/a.cpp
+++ coding_test/a.cpp
@@ -7,7 +7,7 @@ vector<string> split(string input, string delimiter){
     vector<string> ret;
     long long pos;
     string token = "";
-    while((pos=input.find(delimiter))!=string::npos){
+    while((pos=input.find(delimiter)) != string::npos){
         token = input.substr(0, pos);
         ret.push_back(token);
         input.erase(0, pos+delimiter.length());
@@ -46,12 +46,13 @@ int main(){
         cout << "found: " << (*search).first << " " << search->second << "\n";
     }
     cout << "\n";
-    // string s = "Hello, My name is Dongheon Lee!";
-    // string d = " ";
-    // vector<string> splited = split(s, d);
-    // for(string a : splited){
-    //     cout << a << "\n";
-    // }    
+    string s = "Hello, My name is Dongheon Lee!";
+    string d = " ";
+    vector<string> splited = split(s, d);
+    for(auto a : splited){
+        cout << a << "\n";
+    }
+
     return 0;
 }
 
