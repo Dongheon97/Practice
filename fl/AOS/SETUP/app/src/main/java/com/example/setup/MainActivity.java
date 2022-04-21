b680202cae01b050cd4e79733cf4ed4538a7775c
Dongheon97 <jdd04063@gmail.com>
2022-04-21 23:26:50 +0900

    DL4J

---
+++ fl/AOS/SETUP/app/src/main/java/com/example/setup/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.setup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int NUM_SAMPLES = 4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void startBackgroundThread(){
+        Thread thread;
+        thread = new Thread(()-> {
+            createAndUseNetwork();
+        });
+        thread.start();
+    }
+    private void createAndUseNetwork(){
+
+    }
+
+}
